25b3b4ea60a736c89e458d565cc6c9a78b8eaa8a
Fernando <nando4x@gmail.com>
2016-09-16 22:05:17 +0200

    aaa

--- jop/src/main/java/com/nandox/jop/dispatcher/Dispatcher.java
+++ jop/src/main/java/com/nandox/jop/dispatcher/Dispatcher.java
@@ -1,5 +1,24 @@
 package com.nandox.jop.dispatcher;
 
+import java.util.HashMap;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+
 public abstract class Dispatcher {
 
+	protected void initFromServlet(ServletConfig config) throws ServletException {
+		// read parameters and map them
+	}
+
+	protected void initFromFilter(FilterConfig config) throws ServletException {
+		// read parameters and map them
+	}
+	
+	private void initEnv(ServletContext ctx, HashMap params) {
+		// manage parameters
+		
+		// add runtime service servlet
+	}
 }
