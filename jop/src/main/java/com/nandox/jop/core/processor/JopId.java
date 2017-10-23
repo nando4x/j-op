7051d4110b41ffab5141f54f5d2581e0afd2c116
Fernando <nando4x@gmail.com>
2017-10-23 09:06:39 +0200

    ss

--- jop/src/main/java/com/nandox/jop/core/processor/JopId.java
+++ jop/src/main/java/com/nandox/jop/core/processor/JopId.java
@@ -16,13 +16,14 @@ package com.nandox.jop.core.processor;
 public class JopId {
 	private String page;
 	private String Id;
+	private String index;
 	/**
 	 * Constructor
-	 * @param	  CompositId identifier composit ([page].id)
+	 * @param	  CompositId identifier composit ([page].id#index)
 	 * @date      24 gen 2017 - 24 gen 2017
 	 * @author    Fernando Costantino
 	 * @revisor   Fernando Costantino
-	 * @exception ParseException if malformed (must be [page path].idblock)
+	 * @exception ParseException if malformed (must be [page path].idblock#index)
 	 * @return
 	 */
 	public JopId(String CompositId) throws ParseException {
@@ -31,6 +32,13 @@ public class JopId {
 		if ( ini >= 0 && end > ini ) {
 			this.page = CompositId.substring(ini+1, end);
 			this.Id = CompositId.substring(end+2);
+			if ( this.Id.indexOf("#") > 0 ) {
+				try {
+					Integer.valueOf(this.Id.substring(this.Id.indexOf("#")+1));
+					this.index = this.Id.substring(this.Id.indexOf("#")+1);
+					this.Id = this.Id.substring(0, this.Id.indexOf("#"));
+				} catch (Exception e) {}
+			}
 		}// else
 			//throw new ParseException(ErrorsDefine.JOP_ID_MALFORMED);
 	}
@@ -49,6 +57,22 @@ public class JopId {
 		this.Id = Id;
 	}
 	/**
+	 * Constructor
+	 * @param	  PageId page identifier
+	 * @param	  Id block identifier
+	 * @param	  Index optional block index
+	 * @date      24 gen 2017 - 24 gen 2017
+	 * @author    Fernando Costantino
+	 * @revisor   Fernando Costantino
+	 * @exception 
+	 * @return
+	 */
+	public JopId(String PageId, String Id, String Index) {
+		this.page = PageId;
+		this.Id = Id;
+		this.index = Index;
+	}
+	/**
 	 * @return the page
 	 */
 	public String getPage() {
@@ -61,6 +85,6 @@ public class JopId {
 		return Id;
 	}
 	public String composite() {
-		return "["+this.page+"]."+this.Id;
+		return "["+this.page+"]."+this.Id +(this.index!=null&&!this.index.isEmpty()?"#"+this.index:"");
 	}
 }
